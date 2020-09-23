5712e41a3bc3b182900ddc6311b15d0908f73961
imamhsn195 <imamhsn195@gmail.com>
2020-09-23 23:22:43 +0600

    test

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,18 +1,18 @@
 const staticDevCoffee = 'dev-coffee-site-v1';
 const assets = [
-    "/",
-    "index.html",
-    "/css/style.css",
-    "/js/app.js",
-    "/images/coffee1.jpg",
-    "/images/coffee2.jpg",
-    "/images/coffee3.jpg",
-    "/images/coffee4.jpg",
-    "/images/coffee5.jpg",
-    "/images/coffee6.jpg",
-    "/images/coffee7.jpg",
-    "/images/coffee8.jpg",
-    "/images/coffee9.jpg",
+    "https://demo.webtech.app/pwa/",
+    "https://demo.webtech.app/pwa/index.html",
+    "https://demo.webtech.app/pwa/css/style.css",
+    "https://demo.webtech.app/pwa/js/app.js",
+    "https://demo.webtech.app/pwa/images/coffee1.jpg",
+    "https://demo.webtech.app/pwa/images/coffee2.jpg",
+    "https://demo.webtech.app/pwa/images/coffee3.jpg",
+    "https://demo.webtech.app/pwa/images/coffee4.jpg",
+    "https://demo.webtech.app/pwa/images/coffee5.jpg",
+    "https://demo.webtech.app/pwa/images/coffee6.jpg",
+    "https://demo.webtech.app/pwa/images/coffee7.jpg",
+    "https://demo.webtech.app/pwa/images/coffee8.jpg",
+    "https://demo.webtech.app/pwa/images/coffee9.jpg",
 ]
 self.addEventListener("install", installEvent => {
     installEvent.waitUntill(
