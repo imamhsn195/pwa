353d2f7600503ce345f2e5bb725c11b3459cd598
imamhsn195 <imamhsn195@gmail.com>
2020-09-23 22:57:35 +0600

    test

--- serviceWorker.js
+++ serviceWorker.js
@@ -32,6 +32,8 @@ self.addEventListener('fetch', fetchEvent => {
 if("serviceWorker" in navigator){
     window.addEventListener("load", function(){
         navigator.serviceWorker
-        .register("serviceWorker.js").then(res => console.log("Service worker registered")).catch(err => console.log("Service worker no registered", err))
+        .register("serviceWorker.js")
+        .then(res => console.log("Service worker registered"))
+        .catch(err => console.log("Service worker no registered", err))
     })
 }
